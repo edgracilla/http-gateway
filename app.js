479bdc430eb40aca18a97314dcaed05639e16295
akzReekoh <achilles.dinglasan@p3ople4u.com>
2016-01-08 20:06:39 +0800

    REEKOH-251
    -Finished sending of data via HTTP
    -Finished documentation

---
+++ app.js
@@ -0,0 +1,155 @@
+'use strict';
+
+var platform = require('./platform'),
+    isEmpty           = require('lodash.isempty'),
+    isPlainObject = require('lodash.isplainobject'),
+    clients           = {},
+    authorizedDevices = {},
+    server;
+
+platform.on('message', function (message) {
+    if (clients[message.client]) {
+        var client = clients[message.client];
+
+        client.end(new Buffer(message.message));
+
+        platform.sendMessageResponse(message.messageId, 'Message Sent');
+        platform.log(JSON.stringify({
+            title: 'Message Sent',
+            device: message.device,
+            messageId: message.messageId,
+            message: message.message
+        }));
+    }
+});
+
+platform.on('adddevice', function (device) {
+    if (!isEmpty(device) && !isEmpty(device._id)) {
+        authorizedDevices[device._id] = device;
+        platform.log('Successfully added ' + device._id + ' to the pool of authorized devices.');
+    }
+    else
+        platform.handleException(new Error('Device data invalid. Device not added. ' + device));
+});
+
+platform.on('removedevice', function (device) {
+    if (!isEmpty(device) && !isEmpty(device._id)) {
+        delete authorizedDevices[device._id];
+        platform.log('Successfully removed ' + device._id + ' from the pool of authorized devices.');
+    }
+    else
+        platform.handleException(new Error('Device data invalid. Device not removed. ' + device));
+});
+
+platform.once('close', function () {
+	var domain = require('domain');
+	var d = domain.create();
+
+	d.once('error', function(error) {
+		console.error(error);
+		platform.handleException(error);
+		platform.notifyClose();
+		d.exit();
+	});
+
+	d.run(function() {
+		server.exit();
+		platform.notifyClose();
+		d.exit();
+	});
+});
+
+platform.once('ready', function (options, registeredDevices) {
+    var clone   = require('lodash.clone'),
+        domain  = require('domain'),
+        indexBy = require('lodash.indexby'),
+        config  = require('./config.json'),
+        express = require('express'),
+        bodyParser = require('body-parser');
+
+
+    if (!isEmpty(registeredDevices)) {
+        var tmpDevices = clone(registeredDevices, true);
+        authorizedDevices = indexBy(tmpDevices, '_id');
+    }
+
+    if (isEmpty(options.data_topic))
+        options.data_topic = config.data_topic.default;
+
+    if (isEmpty(options.message_topic))
+        options.message_topic = config.message_topic.default;
+
+    if (isEmpty(options.groupmessage_topic))
+        options.groupmessage_topic = config.groupmessage_topic.default;
+
+    server = express();
+    server.use(bodyParser.urlencoded({ extended: false }));
+    server.use(bodyParser.json());
+
+    server.all('/*', function(request, response){
+        var serverDomain = domain.create();
+
+        serverDomain.once('error', function (error) {
+            platform.handleException(error);
+            response.end(new Buffer('Invalid data sent. Must be a valid JSON String.'));
+            serverDomain.exit();
+        });
+
+        serverDomain.run(function(){
+            var url = request.url.split('/')[1];
+            var message;
+
+            if(isPlainObject(request.body))
+                message = request.body;
+            else
+                message = JSON.parse(request.body);
+
+            if(url === options.data_topic){
+                platform.processData(message.device, message);
+                platform.log(JSON.stringify({
+                    title: 'Data Received.',
+                    device: message.device,
+                    data: message
+                }));
+                if (isEmpty(clients[message.device])) {
+                    clients[message.device] = response;
+                }
+            }
+            else if(url === options.message_topic){
+                platform.sendMessageToDevice(message.target, message);
+
+                platform.log(JSON.stringify({
+                    title: 'Message Sent.',
+                    source: message.device,
+                    target: message.target,
+                    message: message
+                }));
+
+                response.end('Message Sent.');
+            }
+            else if(url === options.groupmessage_topic){
+                platform.sendMessageToGroup(message.target, message);
+
+                platform.log(JSON.stringify({
+                    title: 'Message Sent.',
+                    source: message.device,
+                    target: message.target,
+                    message: message
+                }));
+
+                response.end('Message Sent.');
+            }
+            else{
+                response.end('Invalid Topic: '+ url);
+            }
+
+            serverDomain.exit();
+        });
+
+    });
+
+    server.listen(options.port, function(){
+        platform.notifyReady();
+        platform.log('Gateway has been initialized on port ' + options.port);
+    });
+});
---
+++ test/http-gateway.test.js
@@ -0,0 +1,95 @@
+/*
+ * Just a sample code to test the gateway plugin.
+ * Kindly write your own unit tests for your own plugin.
+ */
+'use strict';
+
+const PORT = 8080,
+    DEVICE_ID1 = '567827489028375',
+    DEVICE_ID2 = '567827489028376';
+
+var cp     = require('child_process'),
+	assert = require('assert'),
+    http = require('http'),
+    querystring = require('querystring'),
+	gateway;
+
+describe('Gateway', function () {
+	this.slow(5000);
+
+	after('terminate child process', function () {
+        setTimeout(function(){
+            gateway.kill('SIGKILL');
+        }, 5000);
+
+	});
+
+	describe('#spawn', function () {
+		it('should spawn a child process', function () {
+			assert.ok(gateway = cp.fork(process.cwd()), 'Child process not spawned.');
+		});
+	});
+
+	describe('#handShake', function () {
+		it('should notify the parent process when ready within 5 seconds', function (done) {
+			this.timeout(5000);
+
+			gateway.on('message', function (message) {
+				if (message.type === 'ready')
+					done();
+			});
+
+			gateway.send({
+				type: 'ready',
+				data: {
+                    options: {
+                        port: PORT,
+                        data_topic: 'httpTestData',
+                        message_topic: 'httpTestMessage',
+                        groupmessage_topic: 'httpTestGroupMessage'
+                    },
+                    devices: [{_id: DEVICE_ID1}, {_id: DEVICE_ID2}]
+					}
+			}, function (error) {
+				assert.ifError(error);
+			});
+		});
+	});
+
+	describe('#message', function () {
+		it('should process the message', function (done) {
+            this.timeout(5000);
+
+            var data = JSON.stringify({device: '567827489028375', data: 'test data'});
+
+            var req = http.request({
+                host: '127.0.0.1',
+                path: '/httpTestData',
+                port: PORT,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': Buffer.byteLength(data)
+                }
+            }, function(response){
+                response.on('data', function(chunk){
+                    assert.equal(chunk.toString('utf8'), 'TURNOFF');
+                    done();
+                });
+            });
+            req.write(data);
+            req.end();
+
+            setTimeout(function(){
+                gateway.send({
+                    type: 'message',
+                    data: {
+                        client: '567827489028375',
+                        messageId: '55fce1455167c470abeedae2',
+                        message: 'TURNOFF'
+                    }
+                });
+            }, 2000);
+		});
+	});
+});
