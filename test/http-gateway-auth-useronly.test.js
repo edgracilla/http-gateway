00dcc3028da3a368522189a1f0587b021222aca7
Benj Sicam <bsicam@reekoh.com>
2016-04-17 09:46:53 +0800

    Updated tests

--- test/http-gateway-auth-useronly.test.js
+++ test/http-gateway-auth-useronly.test.js
@@ -97,7 +97,7 @@ describe('HTTP Gateway Auth - User Only', function () {
 			}, function (error, response, body) {
 				assert.ifError(error);
 				assert.equal(200, response.statusCode);
-				assert.equal('OK', body);
+				assert.equal('Data Received', body);
 				done();
 			});
 		});
@@ -119,7 +119,7 @@ describe('HTTP Gateway Auth - User Only', function () {
 			}, function (error, response, body) {
 				assert.ifError(error);
 				assert.equal(200, response.statusCode);
-				assert.equal('OK', body);
+				assert.equal('Message Received', body);
 				done();
 			});
 		});
@@ -141,7 +141,7 @@ describe('HTTP Gateway Auth - User Only', function () {
 			}, function (error, response, body) {
 				assert.ifError(error);
 				assert.equal(200, response.statusCode);
-				assert.equal('OK', body);
+				assert.equal('Group Message Received', body);
 				done();
 			});
 		});
