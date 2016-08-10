daafe3e954325f5d1c2927d6a164efceb6b1cb60
Benj Sicam <benj@codechef.io>
2016-08-10 19:50:05 +0800

    Whitespace

--- app.js
+++ app.js
@@ -208,7 +208,7 @@ platform.once('ready', function (options) {
 
 	app.use((req, res) => {
 		res.set('Content-Type', 'text/plain');
-		
+
 		res.status(404).send(`Invalid Path. ${req.originalUrl} Not Found\n`);
 	});
 
